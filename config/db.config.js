5e9f3ee2f9a8deb8ebd36acbbfb9c911ae9a7da2
Yunus Emre Kavi <ykavi@ebay.com>
2022-01-10 17:13:12 +0300

    remove mongoose

--- config/db.config.js
+++ config/db.config.js
@@ -1,39 +1,26 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 const logger = require('../logger/api.logger');
 
-const connect = () => {
-  const url = process.env.MONGO_CONNECTION_STRING;
+const connect = async () => {
+  const uri = process.env.MONGO_CONNECTION_STRING;
+  const client = new MongoClient(uri);
   logger.info('process.env.MONGO_CONNECTION_STRING :::' + process.env.MONGO_CONNECTION_STRING);
 
-  mongoose.connect(url, {
-    useNewUrlParser: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
-
-  mongoose.connection.once('open', async () => {
-    logger.info('Connected to database');
-  });
-
-  mongoose.connection.on('error', (err) => {
-    logger.error('Error connecting to database  ', err);
-  });
+  try {
+    await client.connect();
+    await listDatabases(client);
+  } catch (e) {
+    logger.error(e);
+  } finally {
+    await client.close();
+  }
 };
 
-const disconnect = () => {
-  if (!mongoose.connection) {
-    return;
-  }
-
-  mongoose.disconnect();
-
-  mongoose.once('close', async () => {
-    console.log('Diconnected  to database');
-  });
+const listDatabases = async (client) => {
+  const databasesList = await client.db().admin().listDatabases();
+  databasesList.databases.forEach((db) => logger.info(`Databases List - ${db.name}`));
 };
 
 module.exports = {
   connect,
-  disconnect,
 };
